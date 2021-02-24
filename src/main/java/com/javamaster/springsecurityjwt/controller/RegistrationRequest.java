2f785fdff626daca8b11683f982b31fa1a69c4a1
w-myslicki <edwa1d.myslicki@gmail.com>
2021-02-24 18:01:55 +0300

    Valid data refactor commit

--- src/main/java/com/javamaster/springsecurityjwt/controller/RegistrationRequest.java
+++ src/main/java/com/javamaster/springsecurityjwt/controller/RegistrationRequest.java
@@ -8,11 +8,6 @@ import javax.validation.constraints.Size;
 @Data
 public class RegistrationRequest {
 
-//    @NotEmpty(message = "Login must not be empty!")
-//    @Size(min = 4, max = 30, message = "Login must be at least 4 and more than 30 characters!")
     private String login;
-
-//    @NotEmpty(message = "Password must not be empty!")
-//    @Size(min = 8, message = "Password must be at least 8 characters long!")
     private String password;
 }
