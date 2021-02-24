433e60854e162588f6688ef224367b38f5cd3680
w-myslicki <edwa1d.myslicki@gmail.com>
2021-02-24 17:10:15 +0300

    Valid data refactor commit

--- src/main/java/com/javamaster/springsecurityjwt/entity/UserEntity.java
+++ src/main/java/com/javamaster/springsecurityjwt/entity/UserEntity.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "user_table")
@@ -21,9 +23,13 @@ public class UserEntity {
     private Integer id;
 
     @Column
+    @NotEmpty(message = "Login must not be empty!")
+    @Size(min = 4, max = 30, message = "Login must be at least 4 and more than 30 characters!")
     private String login;
 
     @Column
+    @NotEmpty(message = "Password must not be empty!")
+    @Size(min = 8, message = "Password must be at least 8 characters long!")
     private String password;
 
     @ManyToOne
