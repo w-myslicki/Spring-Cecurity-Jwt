8ff82e0077c467eba367e085ad9aee1a50feda8e
w-myslicki <edwa1d.myslicki@gmail.com>
2021-02-24 15:43:51 +0300

    Valid data commit

--- src/main/java/com/javamaster/springsecurityjwt/controller/RegistrationRequest.java
+++ src/main/java/com/javamaster/springsecurityjwt/controller/RegistrationRequest.java
@@ -3,13 +3,16 @@ package com.javamaster.springsecurityjwt.controller;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class RegistrationRequest {
 
-    @NotEmpty
+    @NotEmpty(message = "Login must not be empty!")
+    @Size(min = 4, max = 30, message = "Login must be at least 4 and more than 30 characters!")
     private String login;
 
-    @NotEmpty
+    @NotEmpty(message = "Password must not be empty!")
+    @Size(min = 8, message = "Password must be at least 8 characters long!")
     private String password;
 }
